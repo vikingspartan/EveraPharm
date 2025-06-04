b301db426f03f1a679d04ea88279d7d6b7f3a514
vikingspartan <95576516+vikingspartan@users.noreply.github.com>
2025-06-04 18:59:23 -0500

    deploy

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+} 
